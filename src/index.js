4d0967c105f524bad80a99bd59fb70ca84593421
danfrgo <43517221+danfrgo@users.noreply.github.com>
2020-12-30 15:09:09 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express'); // importar express
+const cors = require('cors');
 const server = express(); // iniciar o servidor
 
+server.use(cors());
 server.use(express.json()); // para a API entender o formato JSON
 
 const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes); // intejar o ficheiro com as rotas
 
 
-server.listen(3000, () =>{ // receber requiçoes na porta 3000
+server.listen(3333, () =>{ // receber requiçoes na porta 3000
     console.log('API ONLINE');
 });
